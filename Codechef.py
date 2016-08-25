62ecb0532ec2aee5e41068bc71d9970dc28d9bc1
pchhiller <pulkitpch@gmail.com>
2016-08-25 13:08:51 +0530

    Updated Codechef.py
    Doesnt overwrite existing files. Also keeps telling the status

--- Codechef.py
+++ Codechef.py
@@ -1,7 +1,19 @@
+# -*- coding: UTF-8 -*-
+import pdfkit
 from bs4 import BeautifulSoup
 import urllib2
 import mechanize
+import os
 
+options = {
+    'page-size': 'Letter',
+    'margin-top': '0.75in',
+    'margin-right': '0.75in',
+    'margin-bottom': '0.75in',
+    'margin-left': '0.75in',
+    'encoding': "UTF-8",
+    'no-outline': None
+}
 response = urllib2.urlopen('https://www.codechef.com/problems/school')
 data =response.read()
 start="https://www.codechef.com"
@@ -11,6 +23,10 @@ for link in soup.find_all('tr', class_="problemrow"):
     end=link.a.get('href')
     Url= start+end
     SuccesfulSolutions= link.find('div',style="text-align:center;").string
+    PdfName=SuccesfulSolutions+" "+Name+'.pdf'
+    if (os.path.isfile('./'+PdfName)):
+        print "Skipping" +PdfName+" , because file already exists"
+        continue
     print Url
     br = mechanize.Browser()
     br.set_handle_robots(False)
@@ -23,9 +39,12 @@ for link in soup.find_all('tr', class_="problemrow"):
         if i==2:
             #for child in data1.descendants:
                 #html=html +child.encode('utf-8')
-            print data1
+            #print data1
+            print "Converting to pdf " + PdfName
+            data1= str(data1)
+            html=data1.decode('utf-8')
+            pdfkit.from_string(html,PdfName,options=options)
         i=i+1
-    break
 
 
 
